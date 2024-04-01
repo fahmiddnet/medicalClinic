6457b3ffca8ff3037a5a13b283d52f226bd0d7f6
fahmid <dnetfahmidhasan@gmail.com>
2024-04-01 14:43:42 +0600

    layout design for large screen

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$('.menu-bar').slicknav({
+    prependTo:'.mobilemenu'
+});
+// For colorbox 
+$(document).ready(function(){
+//   $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+  $('.container-fluid .box').matchHeight();
+});
+
+$('#aboutUs').owlCarousel({
+    loop:true,
+    margin:1,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
+
+$('#gallary-car').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:true,
+//   autoWidth:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+
+
+
+
+
+
+
